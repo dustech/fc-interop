a0f7b23520158a0843a7d1e8fcf8cfb592f278c6
dustech <stefano.cerruti@outlook.com>
2024-03-15 10:19:38 +0100

    Add solution and some basic interops

---
+++ src/FCInterop.Consumer/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Threading;
+using Domain;
+using Microsoft.FSharp.Core;
+using static Domain.Users;
+using static Domain.Commands.Filter;
+using static System.Console;
+using static Domain.Commands;
+
+#pragma warning disable CA1303
+
+var exampleUsers = new List<User>()
+{
+    new User("John", "Doe"),
+    new User("Jane", "Doe"),
+    new User("Cannolo", "Siciliano"),
+    new User("Maria", "Rossi"),
+};
+
+// Instantiating UsersInMemory with the example users
+var usersInMemory = toUsersInMemory(exampleUsers);
+
+// Filtering by name
+var filteredUsersByName = usersInMemory.filter(byName("Cannolo"));
+WriteLine("Filtered by Name 'Cannolo':");
+foreach (var user in filteredUsersByName)
+{
+    WriteLine($"Name: {user.Name}, LastName: {user.LastName}");
+}
+
+// Filtering by last name
+
+var filteredUsersByLastName = usersInMemory.filter([
+    byLastName("Doe"),
+    byName("Jane")]
+    );
+WriteLine("\nFiltered by Composite filter :");
+foreach (var user in filteredUsersByLastName)
+{
+    WriteLine($"Name: {user.Name}, LastName: {user.LastName}");
+}
+
+WriteLine("\n Filter by names");
+foreach(var user in usersInMemory.filter(byNames(["jane","John","Pippo"])))
+{
+    WriteLine($"Name: {user.Name}, LastName: {user.LastName}");
+}
+
+WriteLine("\n Filter by predicate");
+{
+    foreach(var user in usersInMemory.filter(byPredicate(λ((User u) => 
+                string.Equals(u.Name, "maria", StringComparison.OrdinalIgnoreCase)
+                ))))
+    {
+        WriteLine($"Name: {user.Name}, LastName: {user.LastName}");
+    }
+}
+usersInMemory.filter(byPredicate(λ<User, bool>(_ => true)));
+#pragma warning restore CA1303
---
+++ src/FCInterop.Domain/Commands.fs
@@ -0,0 +1,64 @@
+﻿namespace Domain
+
+open System
+
+
+
+type User = { Name: string; LastName: string }
+
+module Commands =
+    open System
+
+    type Filter =
+        | ByNames of seq<string>
+        | ByLastnames of seq<string>
+        | ByPredicate of (User -> bool)
+        static member byName value = ByNames [ value ]
+        static member byNames values = ByNames values
+        static member byLastName value = ByLastnames [ value ]
+        static member byLastNames values = ByLastnames values
+        static member byPredicate predicate = ByPredicate predicate
+
+    let λ (func: Func<'a, 'b>) : 'a -> 'b = func.Invoke
+    let lambda = λ
+
+open Commands
+
+module Users =
+
+    // type IUser =
+    //     inherit seq<User>
+    //     abstract getUsers: UserQuery -> seq<User>
+
+    type IUser =
+        inherit seq<User>
+        abstract filter: seq<Filter> -> seq<User>
+        abstract filter: Filter -> seq<User>
+
+    type UsersInMemory(users: seq<User>) =
+        interface IUser with
+            member _.GetEnumerator() = users.GetEnumerator()
+            //(this :> seq<User>).GetEnumerator() :> System.Collections.IEnumerator
+            member this.GetEnumerator() =
+                (this :> seq<User>).GetEnumerator() :> System.Collections.IEnumerator
+
+            member this.filter filters =
+
+                let eq (a: string) (b: string) =
+                    String.Equals(a, b, StringComparison.OrdinalIgnoreCase)
+
+                let rec filter user query =
+                    match query with
+                    | ByNames names -> names |> Seq.exists (eq user.Name)
+                    | ByLastnames lastNames -> lastNames |> Seq.exists (eq user.LastName)
+                    | ByPredicate f -> f user
+
+
+                users
+                |> Seq.filter (fun user ->
+                    filters
+                    |> Seq.fold (fun take query -> take && (filter user query)) true)
+
+            member this.filter filter = [ filter ] |> (this :> IUser).filter
+
+    let toUsersInMemory users : IUser = UsersInMemory users
