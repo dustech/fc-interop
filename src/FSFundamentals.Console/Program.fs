7cc0a54b3f3fb404fe0bf183ea2c6a8b828aa424
dustech <stefano.cerruti@outlook.com>
2024-03-17 18:06:50 +0100

    Add console processor project

---
+++ src/FSFundamentals.Console/Program.fs
@@ -0,0 +1,17 @@
+﻿open System
+
+[<EntryPoint>]
+let main argv = 
+    
+
+
+    printfn "Hello from the transaction processor!"
+
+    printf "(d)eposit, (w)ithdraw or e(x)it: "
+
+    let action = Console.ReadLine()
+
+    printfn "You have selected %A" action
+
+    printfn "Bye!"
+    0
