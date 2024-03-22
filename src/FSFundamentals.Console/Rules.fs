a66dcea48ddd4244732aaf2da0625c11021de917
dustech <stefano.cerruti@outlook.com>
2024-03-22 18:00:39 +0100

    Add transactions to account balance modifications

--- src/FSFundamentals.Console/Rules.fs
+++ src/FSFundamentals.Console/Rules.fs
@@ -1,9 +1,29 @@
 module FSFundamentals.Console.Transaction.Rules
 
+
 module Accounts =
     open FSFundamentals.Console.Transaction.Domain.Account
-    let deposit value account =
-        { account with Balance = account.Balance + value }
+    open FSFundamentals.Console.Transaction.Domain
+    let private nextTransactionId account =
+        match account.Transactions with
+        | [] -> 0
+        | _ ->
+            account.Transactions
+            |> List.map (_.Id)
+            |> List.max
+            |> (+) 1
+    
+    let applyTransaction account amount transaction =
+        {
+            account with
+                Balance = account.Balance + amount
+                Transactions = account.Transactions @ [transaction] 
+        }        
+    
+    let deposit amount account =
+        let newTransaction = Transaction.deposit (nextTransactionId account) amount
+        applyTransaction account amount newTransaction
 
-    let withdraw value account =
-        { account with Balance = account.Balance - value }
+    let withdraw amount account =
+        let newTransaction = Transaction.withdraw (nextTransactionId account) amount
+        applyTransaction account -amount newTransaction
