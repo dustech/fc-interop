3ab7f60bdbabb9f4f2a7205929b2855f073b3d21
dustech <stefano.cerruti@outlook.com>
2024-03-22 17:46:52 +0100

    Add transactions to account domain model

--- src/FSFundamentals.Console/Domain.fs
+++ src/FSFundamentals.Console/Domain.fs
@@ -4,19 +4,19 @@ module Transaction =
     type TransactionType =
         | Deposit
         | Withdraw
-
     type Transaction =
         { Id: int
           Type: TransactionType
           Amount: decimal }
-
     let create id transactionType amount =
         { Id = id
           Type = transactionType
           Amount = amount }
     let deposit id amount = create id Deposit amount 
-    let withdraw id amount = create id Withdraw amount 
+    let withdraw id amount = create id Withdraw amount
+    
 module Account =
-    type Account = { Id: int; Balance: decimal }
-    let create id balance = { Id = id; Balance = balance }
-    let instance = create 0 0m
+    open Transaction
+    type Account = { Id: int; Balance: decimal ; Transactions: Transaction list}
+    let create id balance transactions = { Id = id; Balance = balance; Transactions = transactions }
+    let instance = create 0 0m []
