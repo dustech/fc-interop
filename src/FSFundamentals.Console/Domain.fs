5a13d2b3d324561b7d4f3c4206f8d3f423efc77c
dustech <stefano.cerruti@outlook.com>
2024-03-22 17:41:08 +0100

    Introduce transaction domain model

--- src/FSFundamentals.Console/Domain.fs
+++ src/FSFundamentals.Console/Domain.fs
@@ -1,6 +1,22 @@
 module FSFundamentals.Console.Transaction.Domain
 
-module Account = 
+module Transaction =
+    type TransactionType =
+        | Deposit
+        | Withdraw
+
+    type Transaction =
+        { Id: int
+          Type: TransactionType
+          Amount: decimal }
+
+    let create id transactionType amount =
+        { Id = id
+          Type = transactionType
+          Amount = amount }
+    let deposit id amount = create id Deposit amount 
+    let withdraw id amount = create id Withdraw amount 
+module Account =
     type Account = { Id: int; Balance: decimal }
     let create id balance = { Id = id; Balance = balance }
     let instance = create 0 0m
