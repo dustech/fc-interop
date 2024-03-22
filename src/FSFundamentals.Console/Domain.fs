102e9eb6fc1b86ff606553f0c171cf691e5488a5
dustech <stefano.cerruti@outlook.com>
2024-03-22 17:28:18 +0100

    Reformat Account domain model

--- src/FSFundamentals.Console/Domain.fs
+++ src/FSFundamentals.Console/Domain.fs
@@ -1,5 +1,6 @@
-module FSFundamentals.Console.Transaction.Domain.Account
+module FSFundamentals.Console.Transaction.Domain
 
-type Account = { Id: int; Balance: decimal }
-let create id balance = { Id = id; Balance = balance }
-let instance = create 0 0m
+module Account = 
+    type Account = { Id: int; Balance: decimal }
+    let create id balance = { Id = id; Balance = balance }
+    let instance = create 0 0m
